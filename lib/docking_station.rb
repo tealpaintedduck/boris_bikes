258f2d74cb722f29d8e2fe176b90b3c62db52372
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-21 17:35:37 +0100

    Added number of bikes variable

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,25 @@
 require_relative 'bike'
 
 class DockingStation
+  def initialize
+    @number_of_bikes = 0
+  end
+
+  def number_of_bikes
+    @number_of_bikes
+  end
+
   def release_bike
     fail "No bikes available" unless @bike
     @bike
   end
 
-  def bike_available?
-
-  end
-
   def dock bike
-    @bike = bike
+    if @number_of_bikes == 0
+      @bike = bike
+      @number_of_bikes += 1
+    else
+      fail "There is already a bike docked!"
+    end
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,17 +2,16 @@ require './lib/docking_station.rb'
 
 describe DockingStation do
 
-  it "should check if bike is working" do
-    bike = Bike.new
-    expect(bike).to be_working
-  end
-
-  it "should check if a bike is available" do
-    expect(subject).to respond_to(:bike_available?)
+  it "should initialize with number_of_bikes set to 0" do
+    expect(subject.number_of_bikes).to eq(0)
   end
 
   it "should dock a bike" do
     expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  it "should raise an error when there is already a bike docked" do
+    expect{ subject.dock Bike.new }.to raise_error "There is already a bike docked!"
   end
 
   it "releases working bikes" do
@@ -25,8 +24,6 @@ describe DockingStation do
     it "raises an error when there are no bikes available" do
       expect{ subject.release_bike }.to raise_error "No bikes available"
     end
-
-    it {should respond_to(:release_bike)}
 
     it "should release new bike" do
       subject.dock Bike.new
